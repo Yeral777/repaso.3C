bb261a6317009d0c1567292dba2bcf78c5fe14fa
Yeral777 <144163430+Yeral777@users.noreply.github.com>
2023-09-07 16:07:01 -0300

    Add files via upload

---
+++ Repaso3C/longitud.py
@@ -0,0 +1,14 @@
+def metro_pulgadas(metros):
+    formula = metros * 39.37
+    return f"El valor es {formula} pulgadas"
+
+def metro_pies(pies):
+    formula = pies * 3.28084
+    return f"El valor es {formula} pies"
+
+#Iniciar Git
+# git init
+
+#Añadir archivos
+# git add .
+# git commit -m "mensaje"
---
+++ Repaso3C/peso.py
@@ -0,0 +1,11 @@
+def kilogramos_gramos(gramos):
+    formula = gramos * 1000
+    return f"El valor es {formula} gramos"
+
+def kilogramos_libras(libras):
+    formula = libras * 2.20462
+    return f"El valor es {formula} libras"
+
+def kilogramo_onzas(onzas):
+    formula = onzas * 35.274
+    return f"El valor es {formula} onzas"
